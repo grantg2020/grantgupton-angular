0bd478c4b83b15ae3ff539d8b8d8ca3471b4741c
ghgupton <ghgupton@ncsu.edu>
2023-12-28 12:44:26 -0500

    Fixed failing test case

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactComponent } from './contact.component';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { ButtonComponent } from '../button/button.component';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -8,7 +10,7 @@ describe('ContactComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
+      declarations: [ ContactComponent, NavBarComponent, ButtonComponent ]
     })
     .compileComponents();
 
