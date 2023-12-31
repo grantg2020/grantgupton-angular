08d45ebabdc56f2fe234fabcec77d746bf613269
ghgupton <ghgupton@ncsu.edu>
2023-12-31 15:21:47 -0500

    Removing redundancy, issue is in htaccess after all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideProtractorTestingSupport()],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
